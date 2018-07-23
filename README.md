bd16ea60e31246dff25c6d05a651c2707c2a5650
jimmaaay <thisguy@jimmythompson.me>
2018-07-23 13:58:32 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# simpleWebRTCWrapper
+
+## Commands
+
+* `npm run watch` - Starts a browser sync instance which restarts on any changes.
+* `npm run build` - Generates minified files and places them in the `dist` folder
---
+++ rollup.config.js
@@ -0,0 +1,37 @@
+import resolve from 'rollup-plugin-node-resolve';
+import babel from 'rollup-plugin-babel';
+import commonjs from 'rollup-plugin-commonjs';
+import { uglify } from 'rollup-plugin-uglify';
+import { minify } from 'uglify-js';
+
+export default {
+  input: 'src/index.js',
+  output: {
+    name: 'simpleWebRTCWrapper',
+    file: 'dist/simpleWebRTCWrapper.min.js',
+    sourcemap: true,
+    format: 'umd',
+    banner: '/** @preserve simpleWebRTCWrapper https://github.com/jimmaaay/simple-webrtc-wrapper */',
+  },
+  plugins: [
+    resolve({
+      main: true,
+    }),
+    commonjs({
+      include: 'node_modules/**',
+    }),
+    babel({
+      exclude: 'node_modules/**' // only transpile our source code
+    }),
+    uglify({
+      output: {
+        comments: function(node, comment) {
+          const { value, type } = comment;
+          if (type == "comment2") {
+              return /@preserve|@license|@cc_on/i.test(value);
+          }
+        }
+      }
+    }, minify),
+  ], 
+};
