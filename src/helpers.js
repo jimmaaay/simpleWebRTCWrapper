7b826ab6d176d5aeb94cb39b5b435a4bbba175c5
jimmaaay <thisguy@jimmythompson.me>
2018-07-26 13:54:27 +0100

    wip

---
+++ src/helpers.js
@@ -0,0 +1,15 @@
+// https://stackoverflow.com/questions/34057127/how-to-transfer-large-objects-using-postmessage-of-webworker
+export const convertObjectToArrayBuffer = (obj) => {
+  const string = JSON.stringify(obj);
+  const { buffer } = new TextEncoder(document.characterSet.toLowerCase()).encode(string);
+  return buffer;
+}
+
+// https://stackoverflow.com/questions/34057127/how-to-transfer-large-objects-using-postmessage-of-webworker
+export const arrayBufferToObject = (arrayBuffer) => {
+  const { buffer } = new Uint8Array(arrayBuffer);
+  const decoder = new TextDecoder('utf-8');
+  const view = new DataView(buffer, 0, buffer.byteLength);
+  const string = decoder.decode(view);
+  return JSON.parse(string);
+}
--- src/index.js
+++ src/index.js
@@ -1,57 +1,57 @@
 import EventEmitter from 'eventemitter3';
+import {
+  convertObjectToArrayBuffer,
+  arrayBufferToObject,
+} from './helpers';
 
-const stunServers = [ // TODO: use npm module that has an upto date list
-  'stun.l.google.com:19302',
-  'stun1.l.google.com:19302',
-  'stun2.l.google.com:19302',
-  'stun3.l.google.com:19302',
-  'stun4.l.google.com:19302',
-  'stun.ekiga.net',
-  'stun.ideasip.com',
-  'stun.schlund.de',
-  'stun.stunprotocol.org:3478',
-  'stun.voiparound.com',
-  'stun.voipbuster.com',
-  'stun.voipstunt.com',
-  'stun.voxgratia.org',
-  'stun.services.mozilla.com'
-];
+const defaultOptions = {
+  stunServers: [ // TODO: use npm module that has an upto date list
+    'stun.l.google.com:19302',
+    'stun1.l.google.com:19302',
+    'stun2.l.google.com:19302',
+    'stun3.l.google.com:19302',
+    'stun4.l.google.com:19302',
+    'stun.ekiga.net',
+    'stun.ideasip.com',
+    'stun.schlund.de',
+    'stun.stunprotocol.org:3478',
+    'stun.voiparound.com',
+    'stun.voipbuster.com',
+    'stun.voipstunt.com',
+    'stun.voxgratia.org',
+    'stun.services.mozilla.com'
+  ],
+  sdpConstraints: {
+    optional: [],
+  },
+}
 
-const sdpConstraints = {
-  optional: [],
-};
 
 export default class SimpleWebRTCWrapper extends EventEmitter {
-  constructor(_) {
-    super(_);
+  constructor(options) {
+    super();
 
     this._dataChannel = false;
+    this._connected = false;
 
-    const config = {
+    this.options = { ...defaultOptions, ...options };
+    
+    this.computer = new RTCPeerConnection({
       iceServers: [
         {
-          url: 'stun:' + stunServers[Math.floor(Math.random() * stunServers.length)],
+          url: 'stun:' + this.options.stunServers[Math.floor(Math.random() * this.options.stunServers.length)],
         },
       ],
-    };
-
-    const webConnection = { 
-      optional: [
-        {
-          DtlsSrtpKeyAgreement: true,
-        },
-      ],
-    };
+    }, { optional: [{ DtlsSrtpKeyAgreement: true, }] });
     
-    this.computer = new RTCPeerConnection(config, webConnection);
-    
-    // fires after host has run this.__finishCreatingRoom
+    // fires after host has run this._finishCreatingRoom
     this.computer.addEventListener('datachannel', (e) => {
       const channel = e.channel || e;
       this.dataChannel = channel;
     });
 
-    this.__finishCreatingRoom = this.__finishCreatingRoom.bind(this);
+    this._finishCreatingRoom = this._finishCreatingRoom.bind(this);
+    this._dataChannelOpen = this._dataChannelOpen.bind(this);
   }
 
   get dataChannel() {
@@ -61,32 +61,39 @@ export default class SimpleWebRTCWrapper extends EventEmitter {
   set dataChannel(value) {
     this._dataChannel = value;
     if (! (this._dataChannel instanceof RTCDataChannel)) return;
-    this._dataChannel.addEventListener('open', (e) => { // TODO: remove event listener after being successful
-      this.emit('connected');
-    });
+    this._dataChannel.addEventListener('open', this._dataChannelOpen);
   }
 
-  __finishCreatingRoom(peerOffer) {
+  _finishCreatingRoom(peerOffer) {
     const obj = JSON.parse(atob(peerOffer));
     const answer = new RTCSessionDescription(obj);
     this.computer.setRemoteDescription(answer);
   }
 
+  _dataChannelOpen() {
+    this._connected = true;
+    this._dataChannel.removeEventListener('open', this._dataChannelOpen);
+    this.emit('connected');
+  }
+
+
   createRoom() { // returns a promise resolves to a web rtc connection description, which then will need to be used by the joinRoom function
     return new Promise((resolve, reject) => {
+      const onIcecandidate = () => {
+        if (e.candidate != null) return;
+        const json = JSON.stringify(this.computer.localDescription.toJSON());
+        this.computer.removeEventListener('icecandidate', onIcecandidate);
+        resolve({
+          finishCreatingRoom: this._finishCreatingRoom,
+          offer: btoa(json),
+        });
+      }
+
       this.dataChannel = this.computer.createDataChannel('webrtc', {
         reliable: true,
       });
-
-      this.computer.addEventListener('icecandidate', (e) => { // TODO: remove eventListener after being succesful
-        if (e.candidate != null) return;
-        const json = JSON.stringify(this.computer.localDescription.toJSON());
-        resolve({
-          finishCreatingRoom: this.__finishCreatingRoom,
-          offer: btoa(json),
-        });
-      });
-      
+      this.computer.addEventListener('icecandidate', onIcecandidate);
+    
       this.computer.createOffer()
         .then(_ => this.computer.setLocalDescription(_));
     });
@@ -96,6 +103,12 @@ export default class SimpleWebRTCWrapper extends EventEmitter {
 
   joinRoom(hostOffer) { // returns a promise which resolves to web rtc connection description which the host (person that called createRoom) needs to use to start the connection
     return new Promise((resolve, reject) => {
+      const onIcecandidate = (e) => {
+        if (e.candidate != null) return;
+        this.computer.removeEventListener('icecandidate', onIcecandidate);
+        resolve(btoa(JSON.stringify(this.computer.localDescription)));
+      }
+
       let obj; 
       try {
         obj = JSON.parse(atob(hostOffer));
@@ -109,16 +122,20 @@ export default class SimpleWebRTCWrapper extends EventEmitter {
         .setRemoteDescription(offer)
         .then(_ => {
           return this.computer
-            .createAnswer(sdpConstraints)
+            .createAnswer(this.options.sdpConstraints)
             .then(answerDesc => this.computer.setLocalDescription(answerDesc));
         });
       
-      this.computer.addEventListener('icecandidate', (e) => { // TODO: remove event listener after being succesful
-        if (e.candidate != null) return;
-        resolve(btoa(JSON.stringify(this.computer.localDescription)));
-      });
+      this.computer.addEventListener('icecandidate', onIcecandidate);
       
     });
+  }
+
+  sendObject(obj) {
+    if (this._connected === false) throw new Error('SimpleWebRTCWrapper: No valid connection');
+    if (typeof obj !== 'object') throw new Error('SimpleWebRTCWrapper: No valid object passed to sendObject');
+    const arrayBuffer = convertObjectToArrayBuffer(obj);
+
   }
 
 
